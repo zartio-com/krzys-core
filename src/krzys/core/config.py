92c361ccb3e02f76788889679dd64fc85f1bc649
Patryk <patryk@MacBook-Pro-Patryk.local>
2024-02-18 21:20:07 +0100

    add function for getting file path relative to ~/.krzys

--- src/krzys/core/config.py
+++ src/krzys/core/config.py
@@ -1,7 +1,12 @@
 import os
 
+krzys_path = os.path.expanduser('~/.krzys')
 tmp_path = os.path.expanduser('~/.krzys/tmp')
 
 
 def get_tmp_file(*args) -> str:
     return os.path.join(tmp_path, *args)
+
+
+def file_path(*args) -> str:
+    return os.path.join(krzys_path, *args)
